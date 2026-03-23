d3a0e792b736bc37c9c9905e4c9a420e6f8bdf85
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-03-23 19:39:25 -0400

    fix: cambiando padding-left y padding-right por padding: 15px 0 !important;

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
 @section('content')
-    <div class="content-header" style="padding-left: 0 !important;padding-left: 0 !important;">
+    <div class="content-header" style="padding: 15px 0 !important;">
         <div class="container-fluid" style="--bs-gutter-x: 0rem !important;">
-            <div class="d-flex justify-content-between mt-6">
+            <div class="d-flex justify-content-between mt-3">
                 @yield('header')
             </div>
         </div>
