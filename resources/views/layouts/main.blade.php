04e57e2fa542d5b0368c6cd286ff66ecb4fdbc08
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-03-26 09:41:02 -0400

    correcion de layout.main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,7 @@
 @extends('adminlte::page')
 
 @section('content')
+<div class="container">
     <div class="content-header" style="padding: 15px 0 !important;">
         <div class="container-fluid" style="--bs-gutter-x: 0rem !important;">
             <div class="d-flex justify-content-between mt-3">
@@ -8,6 +9,7 @@
             </div>
         </div>
     </div>
+</div>
 
     @yield('content_body')
 @endsection
