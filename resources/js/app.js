49c203f0352bb0d73fead363ada266f3b6c68c34
Cristian Manuel Abalos Fernandez <cristianmanuel007@gmail.com>
2026-03-27 15:15:55 -0400

    carga y previzualizacion de modelos 3d .glb

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,14 @@
+import Swal from 'sweetalert2';
 import './bootstrap';
-/*import Swal from 'sweetalert2';
+import * as THREE from 'three';
 
-window.Swal = Swal;*/
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
+
+window.Swal = Swal;
+window.THREE = THREE;
+window.GLTFLoader = GLTFLoader;
+window.OrbitControls = OrbitControls;
+window.RGBELoader = RGBELoader;
---
+++ resources/views/components/3d/simple-view/simple-view.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <div id="viewer" class="w-100" style="height: 300px;"></div>
+</div>
