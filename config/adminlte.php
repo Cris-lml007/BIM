c258bcbc8e73dc87797372d496778c20a2763816
vax-02 <javier.xcn.18@gmail.com>
2026-04-06 11:07:25 -0400

    diseño modificado de login y register

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,8 +63,8 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<span class="logo-bim">BIM</span><span class="logo-nova">Nova</span>',
+    'logo_img' => 'vendor/adminlte/dist/img/BIMNOVA.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
